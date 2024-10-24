c702eb557cb0c065c35897877d8f29af10335052
Michał Broda <147035736+kotekloltrzy@users.noreply.github.com>
2024-10-24 18:02:52 +0200

    Add files via upload

---
+++ 24.10.2024/g2174748/src/zad1/Person.java
@@ -0,0 +1,10 @@
+package zad1;
+
+public class Person {
+    public String firstName;
+    public String lastName;
+
+    public Person3(String firstName, String lastName){
+        firstName=firstName;
+        lastName = lastName;}
+}
